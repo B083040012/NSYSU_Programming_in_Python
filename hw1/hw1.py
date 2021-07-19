32a1022ac23281eab55fd2e9f027cc5c600e9567
B083040012 <57710320+B083040012@users.noreply.github.com>
2021-07-19 16:09:22 +0800

    update hw1

---
+++ hw1/hw1.py
@@ -0,0 +1,6 @@
+#B083040012
+x="A quick brown fox jumps over the lazy dog."
+print((sorted(set(x)))[:1-(-1):-1])
+y="print((sorted(set(x)))[:1-(-1):-1])"
+y=y+' '+'!'+'"'
+print((sorted(set(y)))[:1-(-1):-1])
